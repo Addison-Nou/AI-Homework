9d7ddd005b48e072f5f25dd07738bee4713880c6
Alex Nou <nouaddison@gmail.com>
2020-03-15 17:57:12 -0700

    send help

--- proj2/multiagent - student files/multiAgents.py
+++ proj2/multiagent - student files/multiAgents.py
@@ -164,7 +164,146 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        #NOTES:
+
+        #print("--------------------NEW CALL-----------------------")
+
+        #print("Total depth (self.depth): ", self.depth)
+
+        # def minimaxRecursive(aGameState, action, dept, current):
+        #     #print("Current Depth: ", depth)
+        #     if (dept <= self.depth):
+        #         #print("Current depth: ", depth)
+
+        #         #print(str(aGameState))
+        #         # #If we're in a win state
+        #         # if (aGameState.isWin()):
+        #         #     #Stop the player
+        #         #     #return Directions.STOP
+        #         #     print("Win leaf found")
+        #         #     score = self.evaluationFunction(aGameState)
+
+        #         # elif (aGameState.isLose()):
+        #         #     print("Lose leaf found")
+        #         #     score = self.evaluationFunction(aGameState)
+
+        #         for act in aGameState.getLegalActions(0):
+        #             if act != Directions.STOP:
+        #                 #print(str(aGameState))
+        #                 #print("action: {0}".format(act))
+
+        #                 if dept%2 == 0:
+        #                     value = 999999999
+        #                     childScore = minimaxRecursive(aGameState.generateSuccessor(0, act), action, dept+1, value)
+        #                     if childScore[0] > current:
+        #                         current = childScore[0]
+        #                         if dept == 0:
+        #                             action = act
+
+        #                 else:
+        #                     value = -999999999
+        #                     childScore = minimaxRecursive(aGameState.generateSuccessor(0, act), action, dept+1, value)
+        #                     if childScore[0] <= current:
+        #                         current = childScore[0]
+        #                         if dept == 0:
+        #                             action = act
+
+        #         #print("Current Score for action {0}: {1}".format(action, current))
+        #         return (current, action)
+
+        #     else:
+        #         #print("Depth reached; score: ", self.evaluationFunction(aGameState))
+        #         return (self.evaluationFunction(aGameState), action)
+
+        # minimaxResult = minimaxRecursive(gameState, None, 0, -999999999)
+        # #print("FINAL ACTION: ", minimaxResult[1])
+        # #print(self.depth) #This gets passed in to be what depth we search to
+        # #print(self.evaluationFunction) #This is what we call to evaluate what exactly is in the game state
+
+        # return minimaxResult[1]
+
+
+        #Call maximize on the pacman
+            #For each action the pacman takes
+                #Get the potential action for each ghost (Call minimize)
+                #Choose the best (maximum) value for the pacman
+
+
+        #Minimize function
+            #For each ghost except the last
+                #For all actions for that ghost
+                    #Generate the new successor assuming it has moved a given action
+                    #Cafucll Minimize to check against the next ghost
+
+        return self.maximize(gameState, self.depth)[1]
+
+    #maximize the player
+    def maximize(self, aGameState, depth):
+        if aGameState.isLose() or aGameState.isWin() or depth == 0:
+            return self.evaluationFunction(aGameState), None
+            
+        bestAction = None
+        scoreList = []
+        actions = aGameState.getLegalActions(0)
+        
+        #For each legal action for the pacman with the ghost making the worst move
+        for act in aGameState.getLegalActions(0):
+            #Generate the successor for that action
+            successor = aGameState.generateSuccessor(0, act)
+            #Check the best actions for each of the ghosts
+            scoreList.append(self.minimize(successor, 1, depth))
+
+        bestScore = max(scoreList)
+        for scoreIndex in range(len(scoreList)):
+            if scoreList[scoreIndex] == bestScore:
+                bestAction = actions[scoreIndex]
+
+        return (bestScore, bestAction)
+
+
+    #minimize the ghost
+    def minimize(self, aGameState, ghost, depth):
+        if aGameState.isLose() or aGameState.isWin() or depth == 0:
+            return self.evaluationFunction(aGameState)
+            
+        scoreList = []
+
+        #print("Gamestate: ", aGameState)
+        actions = aGameState.getLegalActions(ghost)
+        #print("Ghost: ", ghost)
+        #print("Actions: ", actions)
+
+        #For every ghost except the last ghost (recursive)
+        if (ghost != aGameState.getNumAgents()-1):
+            #print("------------ IF --------------")
+
+            #For every action that the ghost can take
+            for act in aGameState.getLegalActions(ghost):
+                #print("------------ IF FOR --------------")
+                #See the best action that it can take
+                successor = aGameState.generateSuccessor(ghost, act)
+                #Call recursion to check more ghosts
+                scoreList.append(self.minimize(successor, ghost+1, depth))
+
+        #The last ghost
+        else:
+            #print("------------ ELSE --------------")
+            #For every action that it can take (different branches)
+            for act in aGameState.getLegalActions(ghost):
+                #See the best ation that it can take
+                successor = aGameState.generateSuccessor(ghost, act)
+                #Decrement the depth for the next pacman step
+                
+                scoreList.append(self.maximize(successor, depth-1)[0])
+                #print("Scorelist at maximize: ", scoreList)
+                
+
+        #Floating up the recursion function
+        #Find the 'best' value for the ghost and return it
+        #print("End scorelist floating up: ", scoreList)
+        lowest_score = min(scoreList)
+
+        return lowest_score
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -176,7 +315,79 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        return self.maximize(gameState, -999999999, 999999999, self.depth)[1]
+
+    def value(self, aGameState, alpha, beta, depth, agent):
+        if aGameState.isLose() or aGameState.isWin() or depth == 0:
+            return self.evaluationFunction(aGameState), None
+        
+        #Choose whether or not you need to do maximizing or minimizing
+        #If the agent is a ghost then minimize
+        if agent != 0:
+            return self.minimize(aGameState, alpha, beta, depth, agent)
+        else:
+            return self.maximize(aGameState, alpha, beta, depth)
+
+
+    def maximize(self, aGameState, alpha, beta, depth):
+
+        v = -999999999
+        scoreList = []
+        actions = aGameState.getLegalActions(0)
+        bestAction = None
+
+        #for each successor of the state
+        for act in aGameState.getLegalActions(0):
+            #Generate the successor
+            successor = aGameState.generateSuccessor(0, act)
+            #Call value to call minimize
+            # (value, action) = self.value(successor, alpha, beta, depth, 1)
+            value = self.value(successor, alpha, beta, depth, 1)[0]
+            v = max(value, v)
+            #Pruning
+            if v > beta:
+                return (v, None)
+            alpha = max(alpha, v)
+            scoreList.append(value)
+
+        bestScore = max(scoreList)
+        #print("Best Score: ",bestScore)
+        for scoreIndex in range(len(scoreList)):
+            if scoreList[scoreIndex] == bestScore:
+                bestAction = actions[scoreIndex]
+
+        return (bestScore, bestAction)
+
+    def minimize(self, aGameState, alpha, beta, depth, agent):
+
+        v = 999999999
+        scoreList = []
+
+        #Not the last ghost so depth stays the same
+        if (agent != aGameState.getNumAgents()-1):
+
+            #for each successor of the state
+            for act in aGameState.getLegalActions(agent):
+                successor = aGameState.generateSuccessor(agent, act)
+                v = min(self.value(successor, alpha, beta, depth, agent+1)[0], v)
+                if v < alpha:
+                    return (v, None)
+                beta = min(beta, v)
+                scoreList.append(v)
+
+        #The last ghost so we decrement depth and call maximize
+        else:
+            for act in aGameState.getLegalActions(agent):
+                successor = aGameState.generateSuccessor(agent, act)
+                v = min(self.value(successor, alpha, beta, depth-1, 0)[0], v)
+                if v < alpha:
+                    return (v, None)
+                beta = min(beta, v)
+                scoreList.append(v)
+
+        return (min(scoreList), None)
+
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
